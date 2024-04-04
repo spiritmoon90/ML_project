ead8c7e92fc8a0bbd0bc68629d665d8a4a4ddc87
Todd Dai <sushi90926462@gmail.com>
2024-04-03 21:02:19 -0500

    EDA and Training

--- src/exception.py
+++ src/exception.py
@@ -19,9 +19,3 @@ class CustomExpection(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__ == "__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Devide by Zero")
-        raise CustomExpection(e, sys)
