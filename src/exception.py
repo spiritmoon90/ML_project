b3ce580d4c4afe11e5385948c24ba9154b3ef735
Todd Dai <sushi90926462@gmail.com>
2024-04-03 21:01:09 -0500

    EDA and Problem Set

--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,11 @@
 import sys
-import logging
+from src.logger import logging
 def error_message_detail(error, error_detail:sys):
     __, __,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in Python script name [{0}] line number [{1}] error message [{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error)
-    )
+     file_name,exc_tb.tb_lineno,str(error))
+    
         
     
 
@@ -14,7 +14,14 @@ def error_message_detail(error, error_detail:sys):
 
 class CustomExpection(Exception):
     def __init__(self, error_message, error_detial:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail= error_detial)
     def __str__(self):
         return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Devide by Zero")
+        raise CustomExpection(e, sys)
--- src/logger.py
+++ src/logger.py
@@ -15,9 +15,3 @@ logging.basicConfig(
     level = logging.INFO, 
 )
 
-if __name__ == "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise exception.CustomExpection(e, sys)
