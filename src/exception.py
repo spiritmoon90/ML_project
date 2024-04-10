8f97776d98904060196d9089039bdf27904e6cfb
Todd Dai <sushi90926462@gmail.com>
2024-04-09 21:20:01 -0500

    Data Transformation done

--- src/exception.py
+++ src/exception.py
@@ -12,7 +12,7 @@ def error_message_detail(error, error_detail:sys):
     return error_message
     
 
-class CustomExpection(Exception):
+class CustomException(Exception):
     def __init__(self, error_message, error_detial:sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail= error_detial)
