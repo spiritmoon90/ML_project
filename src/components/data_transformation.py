216478feb4ff1ce0f1fb1a7d79b38d17769bdbff
Todd Dai <sushi90926462@gmail.com>
2024-04-11 22:05:31 -0500

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -13,7 +13,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_ob_file_path = os.path.join('artifacts', 'preprocessor.pkl')
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
 
 class DataTransformation:
     def __init__(self):
@@ -83,9 +83,13 @@ class DataTransformation:
             ]
             logging.info(f"Saved preprocessing object.")
             save_object(
-                file_path = self.data_transformation_config.preprocessor_ob_file_path,
+                file_path = self.data_transformation_config.preprocessor_obj_file_path,
                 obj = preprocessing_obj
             )
-
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
         except Exception as e:
             raise CustomException(e, sys)
